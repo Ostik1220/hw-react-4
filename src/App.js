69c676e35868ce6242b4488c5540a117e83ced96
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-10-05 17:42:32 +0300

    format document

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
 import { Statistics } from './Statistics/Statistics';
-import {Friends} from './Friends/Friends'
-import data from './statisticInfo.json'
-import info from './friends.json'
+import { Friends } from './Friends/Friends';
+import data from './statisticInfo.json';
+import info from './friends.json';
 
 function App() {
   return (
-
     <div className="App">
       {/* 1 */}
-              <section className="statistics">
-  <h2 className="title">Upload stats</h2>
-  <ul className="stat-list">
-      <Statistics info={data}/>
+      <section className="statistics">
+        <h2 className="title">Upload stats</h2>
+        <ul className="stat-list">
+          <Statistics info={data} />
+        </ul>
+      </section>
+      <br />
+      {/* 2 */}
+      <ul class="friend-list">
+        <Friends data={info} />
       </ul>
-</section>
-<br/>
-{/* 2 */}
-<ul class="friend-list">
- <Friends data={info}/>
-</ul>
     </div>
-
   );
 }
 
--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import styles from './statistics.module.css';
 export const Statistics = ({ info }) => {
   return (
     <>
-      {info.map((item) => (
+      {info.map(item => (
         <li className={styles.item} key={item.id}>
           <span className={styles.label}>{item.label}</span>
           <span className={styles.percentage}>{item.percentage}%</span>
@@ -11,4 +11,4 @@ export const Statistics = ({ info }) => {
       ))}
     </>
   );
-}
+};
